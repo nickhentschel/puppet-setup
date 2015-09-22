bb8c1c3466128dc9247728f1fed9051cabfcef2f
Nick Hentschel <nhentschel@wayfair.com>
2015-09-22 17:44:30 -0400

    changes to get php-fpm running

--- modules/phpfpm/manifests/config.pp
+++ modules/phpfpm/manifests/config.pp
@@ -1,5 +1,33 @@
 # /etc/puppet/modules/phpfpm/manifests/config.pp
 
 class phpfpm::config {
+  file { '/etc/php-fpm.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/phpfpm/php-fpm.conf',
+    owner   => '0',
+    group   => '0',
+    replace => true,
+    mode    => '0644',
+    notify  => Class['phpfpm::service'],
+  }
 
+  file { '/etc/php.ini':
+    ensure  => file,
+    source  => 'puppet:///modules/phpfpm/php.ini',
+    owner   => '0',
+    group   => '0',
+    replace => true,
+    mode    => '0644',
+    notify  => Class['phpfpm::service'],
+  }
+
+  file { '/etc/php-fpm.d/www.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/phpfpm/www.conf',
+    owner   => '0',
+    group   => '0',
+    replace => true,
+    mode    => '0644',
+    notify  => Class['phpfpm::service'],
+  }
 }
